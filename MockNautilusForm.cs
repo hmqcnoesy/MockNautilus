0aa59664a0d895041739a878f7f418b70293b975
Matthew Mcneil-External <matthew.mcneil@regeneron.com>
2021-04-25 07:33:36 -0400

    inital commit

---
+++ MockNautilusForm.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.Win32;
+using MockNautilus.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MockNautilus
+{
+    public partial class MockNautilusForm : Form
+    {
+        private List<string> _dllsToSkip = new List<string> { "adodb.dll" };
+        private string _registryKey = "MockNautilus";
+
+
+        public MockNautilusForm()
+        {
+            InitializeComponent();
+            LoadLibraries();
+            LoadTypes();
+            LoadSavedParameters();
+        }
+
+
+        private void LoadLibraries()
+        {
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var libraries = Directory.EnumerateFiles(directory, "*.dll");
+            foreach(var lib in libraries)
+            {
+                var fileName = new FileInfo(lib).Name;
+                if (_dllsToSkip.Contains(fileName)) continue;
+
+                cmbLibrary.Items.Add(fileName);
+            }
+        }
+
+
+        private void LoadTypes()
+        {
+            cmbType.Items.Clear();
+            if (string.IsNullOrEmpty(cmbLibrary.Text)) return;
+
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var library = Path.Combine(directory, cmbLibrary.Text);
+
+            var assembly = Assembly.LoadFrom(library);
+            var allTypes = assembly.GetTypes();
+            foreach(var type in allTypes)
+            {
+                if (!type.IsInterface && type.GetInterfaces().Select(i => i.Name).Contains("IEntityExtension"))
+                    cmbType.Items.Add(type);
+            }
+        }
+
+
+        private void Loaded(object sender, EventArgs e)
+        {
+            txtPassword.Select();
+        }
+
+
+        private void ClickedExecute(object sender, EventArgs e)
+        {
+            var type = cmbType.SelectedItem as Type;
+            if (type == null) return;
+
+            var instance = (LSEXT.IEntityExtension)Activator.CreateInstance(type);
+            if (instance == null)
+            {
+                MessageBox.Show("Couldn't create instance of selected type: " + cmbType.Text);
+                return;
+            }
+
+            var parameters = SetupMockParameters();
+            if (parameters != null) ((LSEXT.IEntityExtension)instance).Execute(parameters);
+        }
+
+
+        private LSEXT.LSExtensionParameters SetupMockParameters()
+        {
+            var parameters = new MockExtensionParameters();
+            try
+            {
+                parameters.Product = txtProduct.Text;
+                parameters.Version = txtVersion.Text;
+                parameters.Server = txtServer.Text;
+                parameters.ComputerName = txtComputerName.Text;
+                parameters.HKeyCurrentUser = txtHKeyCurrentUser.Text;
+                parameters.HKeyLocalMachine = txtHKeyLocalMachine.Text;
+                parameters.OperatorName = txtOperatorName.Text;
+                parameters.RoleName = txtRoleName.Text;
+                parameters.OperatorId = decimal.Parse(txtOperatorId.Text);
+                parameters.RoleId = decimal.Parse(txtRoleId.Text);
+                parameters.WorkstationId = decimal.Parse(txtWorkstationId.Text);
+                parameters.SessionId = decimal.Parse(txtSessionId.Text);
+                parameters.Username = txtUsername.Text;
+                parameters.Password = txtPassword.Text;
+                parameters.PasswordRole = txtPasswordRole.Text;
+                parameters.ServerInfo = txtServerInfo.Text;
+
+                parameters.EntityId = decimal.Parse(txtEntityId.Text);
+                parameters.EntityRecords = SplitAndParseEntityRecordsText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Couldn't load given parameters: " + ex.Message);
+                return null;
+            }
+
+            return parameters;
+        }
+
+
+        private ADODB.Recordset SplitAndParseEntityRecordsText()
+        {
+            var parts = txtEntityRecords.Text.Split(";, \t\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var entityRecordIds = new ADODB.Recordset();
+            entityRecordIds.Fields.Append("ID", ADODB.DataTypeEnum.adVariant);
+            entityRecordIds.Open();
+
+            foreach (var id in parts)
+            {
+                entityRecordIds.AddNew("ID", id);
+            }
+
+            if (parts.Length > 0) entityRecordIds.MoveFirst();
+
+            return entityRecordIds;
+        }
+
+
+        private void SaveParameters()
+        {
+            WriteSetting("Library", cmbLibrary.Text);
+            WriteSetting("Type", cmbType.Text);
+            WriteSetting("Product", txtProduct.Text);
+            WriteSetting("Version", txtVersion.Text);
+            WriteSetting("Server", txtServer.Text);
+            WriteSetting("ComputerName", txtComputerName.Text);
+            WriteSetting("HKeyCurrentUser", txtHKeyCurrentUser.Text);
+            WriteSetting("HKeyLocalMachine", txtHKeyLocalMachine.Text);
+            WriteSetting("OperatorName", txtOperatorName.Text);
+            WriteSetting("RoleName", txtRoleName.Text);
+            WriteSetting("OperatorId", txtOperatorId.Text);
+            WriteSetting("RoleId", txtRoleId.Text);
+            WriteSetting("WorkstationId", txtWorkstationId.Text);
+            WriteSetting("SessionId", txtSessionId.Text);
+            WriteSetting("Username", txtUsername.Text);
+            WriteSetting("PasswordRole", System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(txtPasswordRole.Text)));
+            WriteSetting("ServerInfo", txtServerInfo.Text);
+            WriteSetting("EntityId", txtEntityId.Text);
+            WriteSetting("EntityRecords", txtEntityRecords.Text);
+        }
+
+
+        private void WriteSetting(string settingName, string settingValue)
+        {
+            var key = Registry.CurrentUser.OpenSubKey("Software", true);
+            key.CreateSubKey(_registryKey);
+            key = key.OpenSubKey(_registryKey, true);
+            key.SetValue(settingName, settingValue);
+        }
+
+
+        private void LoadSavedParameters()
+        {
+            var isFirstRun = !Registry.CurrentUser.OpenSubKey("Software", false).GetSubKeyNames().Contains(_registryKey);
+            if (isFirstRun)
+            {
+                LoadDefaultParameters();
+                return;
+            }
+
+            cmbLibrary.Text = ReadSetting("Library");
+            cmbType.Text = ReadSetting("Type");
+            txtProduct.Text = ReadSetting("Product");
+            txtVersion.Text = ReadSetting("Version");
+            txtServer.Text = ReadSetting("Server");
+            txtComputerName.Text = ReadSetting("ComputerName");
+            txtHKeyCurrentUser.Text = ReadSetting("HKeyCurrentUser");
+            txtHKeyLocalMachine.Text = ReadSetting("HKeyLocalMachine");
+            txtOperatorName.Text = ReadSetting("OperatorName");
+            txtRoleName.Text = ReadSetting("RoleName");
+            txtOperatorId.Text = ReadSetting("OperatorId");
+            txtRoleId.Text = ReadSetting("RoleId");
+            txtWorkstationId.Text = ReadSetting("WorkstationId");
+            txtSessionId.Text = ReadSetting("SessionId");
+            txtUsername.Text = ReadSetting("Username");
+            txtPasswordRole.Text = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(ReadSetting("PasswordRole")));
+            txtServerInfo.Text = ReadSetting("ServerInfo");
+            txtEntityId.Text = ReadSetting("EntityId");
+            txtEntityRecords.Text = ReadSetting("EntityRecords");
+        }
+
+
+        private void LoadDefaultParameters()
+        {
+            txtProduct.Text = "Nautilus";
+            txtVersion.Text = "9.3";
+            txtComputerName.Text = System.Environment.MachineName;
+            txtHKeyCurrentUser.Text = @"HKEY_CURRENT_USER\Software\Thermo\Nautilus\9.3";
+            txtHKeyLocalMachine.Text = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Thermo\Nautilus\9.3";
+            txtRoleName.Text = "System";
+            txtRoleId.Text = "1";
+            txtUsername.Text = System.Environment.UserName;
+        }
+
+
+        private string ReadSetting(string settingName)
+        {
+            var key = Registry.CurrentUser.OpenSubKey("Software", true);
+            key.CreateSubKey(_registryKey);
+            key = key.OpenSubKey(_registryKey, false);
+            return (string)key.GetValue(settingName);
+        }
+
+
+        private void ClosingForm(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                SaveParameters();
+            }
+            catch
+            {
+                MessageBox.Show("Check that all numeric parameters have numeric values.");
+                e.Cancel = true;
+            }
+        }
+
+        private void cmbLibrary_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadTypes();
+        }
+    }
+}
---
+++ MockParameters.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using LSEXT;
+using LSSERVICEPROVIDERLib;
+
+namespace MockNautilus
+{
+    public class MockExtensionParameters : LSEXT.LSExtensionParameters
+    {
+        public string Product { get; set; }
+        public string Version { get; set; }
+        public string Server { get; set; }
+        public string ComputerName { get; set; }
+        public string HKeyCurrentUser { get; set; }
+        public string HKeyLocalMachine { get; set; }
+        public string OperatorName { get; set; }
+        public string RoleName { get; set; }
+        public decimal OperatorId { get; set; }
+        public decimal RoleId { get; set; }
+        public decimal WorkstationId { get; set; }
+        public decimal SessionId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PasswordRole { get; set; }
+        public string ServerInfo { get; set; }
+
+        public decimal? EntityId { get; set; }
+        public ADODB.Recordset EntityRecords { get; set; }
+
+        public dynamic this[string key]
+        {
+            get { return GetKey(key); }
+        }
+
+        dynamic GetKey(string key)
+        {
+            if (key == "PRODUCT") return Product;
+            if (key == "VERSION") return Version;
+            if (key == "SERVER") return Server;
+            if (key == "COMPUTER_NAME") return ComputerName;
+            if (key == "HKEY_CURRENT_USER") return HKeyCurrentUser;
+            if (key == "HKEY_LOCAL_MACHINE") return HKeyLocalMachine;
+            if (key == "OPERATOR_NAME") return OperatorName;
+            if (key == "ROLE_NAME") return RoleName;
+            if (key == "OPERATOR_ID") return OperatorId;
+            if (key == "ROLE_ID") return RoleId;
+            if (key == "WORKSTATION_ID") return WorkstationId;
+            if (key == "SESSION_ID") return SessionId;
+            if (key == "USERNAME") return Username;
+            if (key == "PASSWORD") return Password;
+            if (key == "PASSWORD_ROLE") return PasswordRole;
+            if (key == "SERVER_INFO") return ServerInfo;
+            if (key == "ENTITY_ID") return EntityId;
+            if (key == "RECORDS") return EntityRecords;
+            if (key == "SERVICE_PROVIDER") return new MockNautilusServiceProvider();
+            throw new ArgumentException();
+        }
+
+        void LSEXT.IExtensionParametersEx.Add(string Key, object Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        int LSEXT.IExtensionParametersEx.Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        System.Collections.IEnumerator LSEXT.IExtensionParametersEx.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        LSEXT.LSExtensionParameter LSEXT.IExtensionParametersEx.Parameter(string Key)
+        {
+            throw new NotImplementedException();
+        }
+
+        void LSEXT.IExtensionParametersEx.Remove(string Key)
+        {
+            throw new NotImplementedException();
+        }
+
+        void LSEXT.IExtensionParametersEx.RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        dynamic IExtensionParametersEx.this[string Key] { get { return GetKey(Key); } set => throw new NotImplementedException(); }
+    }
+
+
+    public class MockNautilusServiceProvider : LSSERVICEPROVIDERLib.NautilusServiceProvider
+    {
+        public dynamic QueryServiceProvider(string serviceProviderName)
+        {
+            if (serviceProviderName == "ProcessXML")
+                return new MockNautilusProcessXML();
+            if (serviceProviderName == "Config")
+                return new MockNautilusConfig();
+            if (serviceProviderName == "Schema")
+                return new MockNautilusSchema();
+            if (serviceProviderName == "PopupMenu")
+                return new MockNautilusPopupMenu();
+
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class MockNautilusProcessXML : LSSERVICEPROVIDERLib.NautilusProcessXML
+    {
+        public string ProcessXML(object xmlDoc)
+        {
+            return string.Empty;
+        }
+
+        public string ProcessXMLFile(string filename)
+        {
+            return string.Empty;
+        }
+
+        public string ProcessXMLWithResponse(object xmlDoc, object xmlResponse)
+        {
+            return string.Empty;
+        }
+
+        public string ProcessXMLFileWithResponse(string filename, object xmlResponse)
+        {
+            return string.Empty;
+        }
+
+        public void SetImportOption(ProcessXMLOption importOption, ProcessXMLSetting importSetting)
+        {
+        }
+
+        public void ResetImportOptions()
+        {
+        }
+    }
+
+
+    public class MockNautilusConfig : NautilusConfig
+    {
+        public bool GetBooleanItem(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStringItem(string itemName)
+        {
+            if (itemName == "System\\Extensions\\PES\\Default Export Directory")
+                return Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (itemName == "System\\Extensions\\PES\\PES Extension Groups")
+                return "PES,TTPE,TTTP";
+
+            if (itemName == "System\\Extensions\\PES\\Workflow for Source Sample")
+                return "PES Source";
+
+            throw new NotImplementedException();
+        }
+
+        public double GetDoubleItem(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckItemExists(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckSecurity(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class MockNautilusSchema : NautilusSchema
+    {
+        public double EntityIdFromName(string entityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EntityNameFromId(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EntityIconNames(double EntityID, out string selectedIcon, out string unselectedIcon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetEntityTableId(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetEntityKeyFieldId(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetEntityNameFieldId(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetEntityFilterId(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTableDatabaseName(double tableId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFieldDatabaseName(double fieldId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTableExtraTableId(double tableId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTableKeyFieldId(double tableId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetFieldTableId(double fieldId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckFieldInTable(string tableName, string fieldName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTableIdFromDatabaseName(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetTableAllowVersioning(double tableId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEntitySecurityItemPrefix(double EntityID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class MockNautilusPopupMenu : NautilusPopupMenu
+    {
+        public void PopupMenu(double EntityID, string recordIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopupMenuAtPosition(double EntityID, string recordIds, int screenX, int screenY)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopupMenuSelectDefault(double EntityID, string recordIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopupMenuSelectByName(double EntityID, string recordIds, string itemName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
